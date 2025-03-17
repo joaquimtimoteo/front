ef1bb52a87f3ba0120b2e499cdfb495a7d862cda
Joaquim Timóteo <109276363+joaquimtimoteo@users.noreply.github.com>
2025-03-17 21:54:19 +0300

    Update MyForm.cpp

--- MyForm.cpp
+++ MyForm.cpp
@@ -1,128 +1,106 @@
 #include "MyForm.h"
 
-Лабораторная работа №9.2
-
-
-using namespace Lab;
 using namespace System;
 using namespace System::Windows::Forms;
 
-// Função que calcula ln(1+x) usando a expansão em série
-double MyForm::CalcularSerie(double x)
-{
-    double soma = 0;
-    int i = 1;
-    double termo = 0;
-    // Calcula os termos até que o valor absoluto do termo seja menor que 0.0001
-    do {
-        // Calcula o termo: x^i / i
-        termo = Math::Pow(x, i) / i;
-        // Alterna o sinal: positivo para i ímpar, negativo para i par
-        if (i % 2 == 0)
-            termo = -termo;
-        soma += termo;
-        i++;
-    } while (Math::Abs(termo) >= 0.0001);
-    return soma;
-}
-
-// Função que utiliza a função da biblioteca para calcular ln(1+x)
-double MyForm::CalcularFuncao(double x)
-{
-    return Math::Log(1 + x);
-}
-
-// Evento do botão "Calcular"
-System::Void MyForm::cmdCalc_Click(System::Object^ sender, System::EventArgs^ e)
-{
-    double x;
-    if (!Double::TryParse(txtX->Text, x))
-    {
-        MessageBox::Show("Por favor, insira um número válido.");
-        return;
+namespace Lab10 {
+    MyForm::MyForm(void) {
+        InitializeComponent();
     }
-    
-    double resultadoSerie = CalcularSerie(x);
-    txtResult->Text = resultadoSerie.ToString("F4"); // Formata com 4 casas decimais
 
-    double resultadoLib = CalcularFuncao(x);
-    txtLib->Text = resultadoLib.ToString("F4");
+    void MyForm::InitializeComponent() {
+        // Configuração da interface (mesmo código do exemplo anterior)
+        // ... (código da interface omitido para brevidade)
+    }
 
-    double diferenca = Math::Abs(resultadoSerie - resultadoLib);
-    lblDiff->Text = "Diferença: " + diferenca.ToString("F4");
+    // Implementação das funções
+    bool MyForm::GetInt(int MaxVal, TextBox^ textBox, String^ errorMessage, int% value) {
+        try {
+            int num = Convert::ToInt32(textBox->Text);
+            if (num < 1 || num > MaxVal) throw gcnew Exception();
+            value = num;
+            return true;
+        } catch (...) {
+            MessageBox::Show(errorMessage);
+            return false;
+        }
+    }
 
-    return;
+    void MyForm::input(array<int>^ mas, int n, int a, int b) {
+        Random^ rnd = gcnew Random();
+        for (int i = 0; i < n; i++) {
+            mas[i] = rnd->Next(a, b + 1);
+        }
+    }
+
+    bool MyForm::isTwoDigit(int num) {
+        int absNum = abs(num);
+        return absNum >= 10 && absNum <= 99;
+    }
+
+    double MyForm::calculateAverage(array<int>^ mas, int n) {
+        int sum = 0, count = 0;
+        for (int i = 0; i < n; i++) {
+            if (isTwoDigit(mas[i])) {
+                sum += mas[i];
+                count++;
+            }
+        }
+        return count == 0 ? 0 : static_cast<double>(sum) / count;
+    }
+
+    int MyForm::task(array<int>^ mas, int n, double avg, array<int>^% newMas) {
+        int k = 0;
+        for (int i = 0; i < n; i++) {
+            if (mas[i] > avg) {
+                newMas[k++] = mas[i];
+            }
+        }
+        return k;
+    }
+
+    void MyForm::output(array<int>^ mas, int n, ListBox^ listBox) {
+        listBox->Items->Clear();
+        for (int i = 0; i < n; i++) {
+            listBox->Items->Add(mas[i].ToString());
+        }
+    }
+
+    // Eventos dos botões
+    void MyForm::btnSolve_Click(Object^ sender, EventArgs^ e) {
+        int n, a, b;
+        if (!GetInt(100, txtN, "Tamanho inválido (1-100)!", n) ||
+            !GetInt(1000, txtA, "Intervalo inválido!", a) ||
+            !GetInt(1000, txtB, "Intervalo inválido!", b) || a >= b) {
+            MessageBox::Show("Dados de entrada inválidos!");
+            return;
+        }
+
+        array<int>^ original = gcnew array<int>(n);
+        input(original, n, a, b);
+        output(original, n, lstOriginal);
+
+        double avg = calculateAverage(original, n);
+        array<int>^ newMas = gcnew array<int>(n);
+        int newSize = task(original, n, avg, newMas);
+
+        if (newSize > 0) {
+            output(newMas, newSize, lstNew);
+        } else {
+            MessageBox::Show("Nenhum elemento maior que a média!");
+        }
+    }
+
+    void MyForm::btnExit_Click(Object^ sender, EventArgs^ e) {
+        this->Close();
+    }
 }
 
-// Evento do botão "Sair"
-System::Void MyForm::cmdExit_Click(System::Object^ sender, System::EventArgs^ e)
-{
-    this->Close();
-    return;
-}
-
-// Função main permanece inalterada
-void main()
-{
+[STAThread]
+int main(array<String^>^ args) {
     Application::EnableVisualStyles();
     Application::SetCompatibleTextRenderingDefault(false);
-    Application::Run(gcnew MyForm());
-}
-
---------
-
-
-
-
-    #include "MyForm.h"
-using namespace Lab;
-using namespace System;
-using namespace System::Windows::Forms;
-
-// Cálculo da série alternada para ln(1+x)
-double MyForm::CalcularSerie(double x) {
-    double soma = 0;
-    int i = 1;
-    double termo;
-    do {
-        termo = Math::Pow(x, i) / i;
-        if (i % 2 == 0) termo *= -1; // Alterna o sinal
-        soma += termo;
-        i++;
-    } while (Math::Abs(termo) >= 0.0001); // Critério de parada
-    return soma;
-}
-
-// Cálculo usando a função da biblioteca
-double MyForm::CalcularFuncao(double x) {
-    return Math::Log(1 + x);
-}
-
-// Evento do botão "Calcular"
-Void MyForm::cmdCalc_Click(Object^ sender, EventArgs^ e) {
-    double x;
-    if (!Double::TryParse(txtX->Text, x) || x <= -1 || x > 1) {
-        MessageBox::Show("Insira um número entre (-1, 1].");
-        return;
-    }
-
-    double serie = CalcularSerie(x);
-    double biblioteca = CalcularFuncao(x);
-    double diferenca = Math::Abs(serie - biblioteca);
-
-    txtResult->Text = serie.ToString("F4");
-    txtLib->Text = biblioteca.ToString("F4");
-    lblDiff->Text = "Diferença: " + diferenca.ToString("F4");
-}
-
-// Evento do botão "Sair"
-Void MyForm::cmdExit_Click(Object^ sender, EventArgs^ e) {
-    this->Close();
-}
-
-// Função Main
-int main() {
-    Application::EnableVisualStyles();
-    Application::Run(gcnew MyForm());
+    Lab10::MyForm form;
+    Application::Run(%form);
     return 0;
 }
