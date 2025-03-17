4a13e6ee25ff4692b42b9b8cb7a451731168d4ce
Joaquim Timóteo <109276363+joaquimtimoteo@users.noreply.github.com>
2025-03-17 18:21:19 +0300

    Update and rename main.cpp to MyForm.cpp

---
+++ MyForm.cpp
@@ -0,0 +1,11 @@
+#include "MyForm.h"
+
+using namespace Lab;
+
+void main() {
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	Application::Run(gcnew MyForm());
+}
+
+
