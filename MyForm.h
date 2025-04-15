de25826d20fae78c253592eadcdafb09f375455a
Joaquim Timóteo <109276363+joaquimtimoteo@users.noreply.github.com>
2025-04-15 20:07:35 +0300

    Update MyForm.h

--- MyForm.h
+++ MyForm.h
@@ -5,14 +5,15 @@ namespace Lab12Variante24 {
     using namespace System;
     using namespace System::Windows::Forms;
     using namespace System::Drawing; // Para Point e Size
-    #include <cstdlib> // Para srand, rand
+    #include <cstdlib> // Para rand
     #include <ctime>   // Para time
 
     public ref class MyForm : public Form {
     public:
         MyForm(void) {
             InitializeComponent();
-            components = nullptr; // Inicializa components
+            components = nullptr;
+            srand(static_cast<unsigned int>(time(nullptr))); // Inicializa srand uma vez
         }
 
     protected:
@@ -21,8 +22,7 @@ namespace Lab12Variante24 {
         }
 
     private:
-        // Declaração dos elementos do formulário
-        System::ComponentModel::Container^ components; // Variável components
+        System::ComponentModel::Container^ components;
         TextBox^ textM;
         TextBox^ textN;
         TextBox^ textA;
@@ -87,11 +87,9 @@ namespace Lab12Variante24 {
             this->labelMatr2->Location = Point(304, 122);
             this->labelMatr2->Text = "Matriz Processada:";
 
-            // Eventos dos botões
             this->btnTask->Click += gcnew EventHandler(this, &MyForm::btnTask_Click);
             this->btnExit->Click += gcnew EventHandler(this, &MyForm::btnExit_Click);
 
-            // Adicionar elementos ao formulário
             this->Controls->Add(textM);
             this->Controls->Add(textN);
             this->Controls->Add(textA);
@@ -107,15 +105,12 @@ namespace Lab12Variante24 {
             this->Controls->Add(labelMatr1);
             this->Controls->Add(labelMatr2);
 
-            // Configurações do formulário
             this->Text = "Lab12 Variante 24";
             this->ClientSize = Size(450, 280);
         }
 
-        // Função para criar uma matriz dinâmica com números aleatórios
         double** CreateMatrix(int m, int n, double a, double b) {
             double** matrix = new double* [m];
-            srand(static_cast<unsigned int>(time(nullptr))); // Corrigido
             for (int i = 0; i < m; i++) {
                 matrix[i] = new double[n];
                 for (int j = 0; j < n; j++) {
@@ -125,19 +120,17 @@ namespace Lab12Variante24 {
             return matrix;
         }
 
-        // Função para exibir uma matriz em um ListBox
         void OutputMatrix(double** matrix, int m, int n, ListBox^ listBox) {
             listBox->Items->Clear();
             for (int i = 0; i < m; i++) {
                 String^ row = "";
                 for (int j = 0; j < n; j++) {
-                    row += matrix[i][j].ToString("F2") + " ";
+                    row += matrix[i][j].ToString("F2") + "\t";
                 }
                 listBox->Items->Add(row);
             }
         }
 
-        // Função para liberar a memória alocada para a matriz
         void FreeMatrix(double** matrix, int m) {
             for (int i = 0; i < m; i++) {
                 delete[] matrix[i];
@@ -145,7 +138,6 @@ namespace Lab12Variante24 {
             delete[] matrix;
         }
 
-        // Função para encontrar os índices dos elementos máximo e mínimo na matriz
         void FindMinMax(double** matrix, int m, int n, int& i_max, int& j_max, int& i_min, int& j_min) {
             double max_val = matrix[0][0], min_val = matrix[0][0];
             i_max = j_max = i_min = j_min = 0;
@@ -166,18 +158,15 @@ namespace Lab12Variante24 {
             }
         }
 
-        // Função para trocar linhas ou colunas conforme especificado
         void SwapRowsOrColumns(double** matrix, int m, int n) {
             int i_max, j_max, i_min, j_min;
             FindMinMax(matrix, m, n, i_max, j_max, i_min, j_min);
 
             if (i_max != i_min) {
-                // Trocar linhas
                 double* temp = matrix[i_max];
                 matrix[i_max] = matrix[i_min];
                 matrix[i_min] = temp;
             } else {
-                // Trocar colunas
                 for (int i = 0; i < m; i++) {
                     double temp = matrix[i][j_max];
                     matrix[i][j_max] = matrix[i][j_min];
@@ -186,33 +175,39 @@ namespace Lab12Variante24 {
             }
         }
 
-        // Evento do botão "Решить"
         void btnTask_Click(Object^ sender, EventArgs^ e) {
             try {
-                // Entrada de dados
+                if (String::IsNullOrWhiteSpace(textM->Text) || String::IsNullOrWhiteSpace(textN->Text) ||
+                    String::IsNullOrWhiteSpace(textA->Text) || String::IsNullOrWhiteSpace(textB->Text)) {
+                    MessageBox::Show("Por favor, preencha todos os campos.");
+                    return;
+                }
+
                 int m = Convert::ToInt32(textM->Text);
                 int n = Convert::ToInt32(textN->Text);
                 double a = Convert::ToDouble(textA->Text);
                 double b = Convert::ToDouble(textB->Text);
 
-                // Chamada das funções
+                if (m <= 0 || n <= 0) {
+                    MessageBox::Show("O número de linhas e colunas deve ser maior que zero.");
+                    return;
+                }
+
+                if (a >= b) {
+                    MessageBox::Show("O limite inferior (a) deve ser menor que o limite superior (b).");
+                    return;
+                }
+
                 double** matrix = CreateMatrix(m, n, a, b);
                 OutputMatrix(matrix, m, n, listBox1);
-
-                // Processamento
                 SwapRowsOrColumns(matrix, m, n);
-
-                // Saída
                 OutputMatrix(matrix, m, n, listBox2);
-
-                // Liberação de memória
                 FreeMatrix(matrix, m);
             } catch (Exception^ ex) {
                 MessageBox::Show("Erro: " + ex->Message);
             }
         }
 
-        // Evento do botão "Выход"
         void btnExit_Click(Object^ sender, EventArgs^ e) {
             this->Close();
         }
